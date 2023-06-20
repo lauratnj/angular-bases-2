dd39350c7cf0263261432ac92ed4647fac1ed6a6
Laura Picon Espegel <laura_picon@innotecsystem.com>
2023-06-20 16:42:44 +0200

    Fin de la sección 8

--- src/app/shared/components/lazy-image/lazy-image.component.ts
+++ src/app/shared/components/lazy-image/lazy-image.component.ts
@@ -1,9 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { timeout } from 'rxjs';
 
 @Component({
   selector: 'shared-lazy-image',
   templateUrl: './lazy-image.component.html',
 })
-export class LazyImageComponent {
+export class LazyImageComponent implements OnInit {
+
+  @Input()
+  public url!:string;
+
+  @Input()
+  public alt: string = '';
+
+  public hasLoaded: boolean = false;
+
+  ngOnInit(): void {
+    if ( !this.url ) throw new Error('URL property is required');
+  }
+
+/*   onLoad() {
+    setTimeout(() => {
+      this.hasLoaded = true;
+
+    }, 1000);
+  } */
+
+  onLoad() {
+    this.hasLoaded = true;
+  }
 
 }
